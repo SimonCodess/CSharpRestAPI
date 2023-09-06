3a88b70c5ea6ba9871072cd5743905d2385b2b37
Šimon Vepřek <81298182+SimonCodess@users.noreply.github.com>
2023-09-06 16:44:54 +0200

    Delete Responses directory

--- Responses/GarageResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace RestAPI_Work.Responses
-{
-    public class GarageResponse
-    {
-        public int GarageId { get; set; }
-        public string? Name { get; set; }
-        public ICollection<MachineResponse>? Machines { get; set; }
-    }
-}
--- Responses/MachineResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace RestAPI_Work.Responses
-{
-    public class MachineResponse
-    {
-        public int MachineId { get; set; }
-        public string Name { get; set; } = "";
-        public int GarageId { get; set; }
-    }
-}
